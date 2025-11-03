515fd4052529a3e92896b9b4a93f132418fb1745
Camilo Ramos <cramoss91@gmail.com>
2025-11-03 18:43:47 -0300

    umm

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,11 +5,15 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength;
+    [SerializeField] float rotationStrength;
     Rigidbody _rigidbody;
 
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void Start()
@@ -17,11 +21,39 @@ public class Movement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("WICKED");
+            _rigidbody.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    void ProcessRotation()
+    {
+       float rotationInput = rotation.ReadValue<float>();
+
+       // For some reason the inputs are reverted, maybe because of the X axis origin which is
+       // pointing to the left arrow by default.
+       if (rotationInput < 0)
+       {
+           ApplyRotation(Vector3.right);
+       }
+       
+       if (rotationInput > 0)
+       {
+           ApplyRotation(Vector3.left);
+       }
+    }
+
+    private void ApplyRotation(Vector3 rotationDirection)
+    {
+        transform.Rotate(rotationDirection * rotationStrength * Time.fixedDeltaTime);
     }
 }
